80b879ef9965f62aca07766e4a0d41c2aaff5129
habbkad <habibkadiri52@gmail.com>
2023-11-09 18:30:56 +0000

    lyfe cycle

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import axios from "axios";
 
 function About() {
-  return <div>About</div>;
+  const [city, setCity] = useState("");
+  const [cityWeather, setCityWeather] = useState("");
+
+  let apiKey = "573923e4b66e90258aa72bdf90b33b01";
+
+  const getWeather = async () => {
+    await axios(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => {
+          setCity(e.target.value);
+        }}
+      />
+      <br />
+      <Button onClick={getWeather}>Search</Button>
+      <h1>{cityWeather}</h1>
+    </div>
+  );
 }
 
 export default About;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 function Home() {
   const navigate = useNavigate();
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      //using fetch
+      // await fetch("https://jsonplaceholder.typicode.com/posts")
+      //   .then((response) => response.json())
+      //   .then((json) => {
+      //     console.log(json);
+      //  setApiData(json)
+      //   });
+
+      //using axios
+      await axios("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => {
+          console.log(response.data);
+          setApiData(response.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getData();
+  }, []);
+
   return (
     <div>
       Home
@@ -12,6 +38,15 @@ function Home() {
       >
         Products
       </button>
+      {apiData.map((item) => {
+        return (
+          <div>
+            <h1>{item.title}</h1>
+            <p>{item.body}</p>
+            <hr />
+          </div>
+        );
+      })}
     </div>
   );
 }
